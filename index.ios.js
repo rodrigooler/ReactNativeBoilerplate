8ae38d5690be3ef966a0e206b3128402eb841ec7
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-02-10 23:10:48 -0200

    Create project

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native Boilerplate App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import {AppRegistry} from 'react-native';
+import ReactNativeBoilerplate from './src';
+
+AppRegistry.registerComponent('ReactNativeBoilerplate', () => ReactNativeBoilerplate);
---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native Boilerplate App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import HelloWorld from './components/HelloWorld';
+
+export default class ReactNativeBoilerplate extends Component {
+  render() {
+    return (
+      <HelloWorld />
+    );
+  }
+}
